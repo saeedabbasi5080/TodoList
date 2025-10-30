26889bf8962377d56969df9356ea91e5bc9c7783
saeid <saeid@DESKTOP-DIP5DH6>
2025-10-30 18:07:07 +0330

    Add project files.

---
+++ Presentation/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using Infrastructure;
+using Application;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Builder;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "Host = localhost; Database=Crud;Username=postgres;Password=1001";
+
+builder.Services.AddDbContext<AppDbContext>(Opt => Opt.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<ITodoRepository, ToDoRepository>();
+builder.Services.AddScoped<TodoService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+       var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
---
+++ Presentation/TodoController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoService _service;
+
+        public TodoController(TodoService todoService) => _service = todoService;
+        //{
+        //    _service = todoService;
+        //}
+
+        [HttpGet]
+        public async Task<ActionResult<List<TodoDto>>> Get() => Ok(await _service.GetAllAsync());
+        //{
+        //    return Ok(await _service.GetAllAsync());
+        //}
+
+        [HttpGet("id")]
+        public async Task<ActionResult<TodoDto?>> GetById(int id)
+        {
+            var item = await _service.GetByIdAsync(id);
+            if (item is null)
+                return NotFound();
+
+            return Ok(item);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoDto>> Post([FromBody] TodoDto dto)
+        {
+            var item = await _service.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("id")]
+        public async Task<ActionResult> Put(int id, [FromBody] TodoDto dto)
+        {
+            if (id != dto.Id) return BadRequest();
+            await _service.UpdateAsync(id, dto);
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
